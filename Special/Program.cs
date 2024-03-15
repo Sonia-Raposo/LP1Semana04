9c8da4617d967b20bb0877e9f1849c61756ff331
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-03-15 17:26:03 +0000

    Created method GetSpecial with exception for 0 and 1

--- Special/Program.cs
+++ Special/Program.cs
@@ -8,5 +8,30 @@ namespace Special
         {
             Console.WriteLine("Hello, World!");
         }
+
+        private static int GetSpecial(int n) 
+        {
+
+            int calc; 
+
+            //exception for 0 
+            if (n== 0)
+            {
+                calc = 0;
+            } 
+
+            //exception for 1
+            else if (n == 1)
+            {
+                calc = 1;
+            }
+
+            //all other numbers
+            else
+            {         
+                calc = GetSpecial(n-1) + 2 * GetSpecial(n-2);
+            }
+         
+            return calc;
     }
 }
