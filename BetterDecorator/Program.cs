52ea87fa4be3ff1862bbae9ca8c58e5a2ddf9cad
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-03-15 17:12:02 +0000

    Created main Decor method

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Xml.XPath;
 
 namespace BetterDecorator
 {
@@ -7,6 +9,34 @@ namespace BetterDecorator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+
+        private static string Decor(string s, char decor, int i)
+        {
+            
+            string result = "";
+
+            // adding on left side
+            for (int a = 0; a < i; a++)
+            {
+                result += decor;
+            }
+
+            result += " " + s + " ";
+
+            // adding on right side
+            for (int b = 0; b < i; b++)
+            {
+                result += decor;
+            }
+
+            return result;
+
+
+
+                
+
+
         }
     }
 }
